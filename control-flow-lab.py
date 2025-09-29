798ff892fdc3317d3f3ca2d7f8e7d43b0fbd37af
visdomoyat <wisdomoyatokun@gmail.com>
2025-09-29 09:28:13 -0400

    exercises to practice control flow in python

---
+++ control-flow-lab.py
@@ -0,0 +1,9 @@
+
+def print_greeting():
+    # Your code goes here. Remember to indent!
+    python_is_fun = True
+    if python_is_fun:
+        print("Python is fun!")
+
+# Call the function
+print_greeting()
