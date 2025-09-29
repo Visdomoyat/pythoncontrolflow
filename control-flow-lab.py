48a5724b48a59befb025b6d8b16e9b16c36af6b5
visdomoyat <wisdomoyatokun@gmail.com>
2025-09-29 10:36:34 -0400

    inputs method exercise

--- control-flow-lab.py
+++ control-flow-lab.py
@@ -7,3 +7,36 @@ def print_greeting():
 
 # Call the function
 print_greeting()
+
+def check_letter():
+    # Your control flow logic goes here
+    letter = input("Enter a letter: ").strip().casefold()
+    if letter in "aeiou":
+        print(f"{letter} is a vowel.")
+    elif letter in "bcdfghjklmnpqrstvwxyz":
+        print(f"{letter} is a consonant.")
+    else:
+        print(f"{letter} is not a letter, please enter a letter.")
+        
+# Call the function
+check_letter()
+
+def check_voting_eligibility():
+    # Your control flow logic goes here
+    age = input("Enter your age: ").strip()
+    if not age.isdigit():
+        print("Please enter a valid age.")
+        return
+    if int(age) >= 18:
+        print("You are eligible to vote.")
+    else:
+        print("You are not eligible to vote.")
+    
+# Call the function
+check_voting_eligibility()
+
+def calculate_dog_years():
+    # Your control flow logic goes here
+
+# Call the function
+calculate_dog_years()
