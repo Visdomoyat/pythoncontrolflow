b5dd8b691c63656dc522ecfab0c9d34562546490
visdomoyat <wisdomoyatokun@gmail.com>
2025-09-30 11:23:06 -0400

    Logical operators

--- control-flow-lab.py
+++ control-flow-lab.py
@@ -37,6 +37,50 @@ check_voting_eligibility()
 
 def calculate_dog_years():
     # Your control flow logic goes here
-
+    dog_age = input("Enter your dog's age in human years: ").strip()
+    if not dog_age.isdigit():
+        print("Please enter a valid age.")
+        return
+    if int(dog_age) < 0:
+        print("Age cannot be negative.")
+        return
+    if int(dog_age) == 2:
+        human_years = int(dog_age) * 10
+        print(f"Your dog is {human_years} years old in human years.")
+    else:
+        int(dog_age) > 2
+        human_years = 20 + (int(dog_age) - 2) * 7
+        print(f"Your dog is {human_years} years old in human years.")
+    
 # Call the function
 calculate_dog_years()
+
+def weather_advice():
+    # Your control flow logic goes here
+    cold = input("Enter (yes/no) if it is cold: ").strip().casefold()
+    raining = input("Enter (yes/no) if it is raining: ").strip().casefold()
+    if cold == "yes" and raining == "yes":
+        print("Wear a waterproof coat.")
+    elif cold =="yes" and raining == "no":
+        print("Wear a warm coat.")
+    elif cold =="no" and raining == "yes":
+        print("Carry an umbrella")
+    else:
+        print("Wear light clothing.")
+    
+# Call the function
+weather_advice()
+
+def determine_season():
+    # Your control flow logic goes here
+    valid_months =["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    while True:
+        month = input("Enter month (3 character, e.g Jan, Feb, Mar): ").strip().title()
+        if len(month) in valid_months:
+            return
+            break
+        else:
+            print("Please enter the first three letters of the month")
+        day = input("Enter day ()")
+# Call the function
+determine_season()
